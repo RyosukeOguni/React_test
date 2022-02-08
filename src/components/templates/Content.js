4075457685e8bbbf5b2e07cc9970856d99ca8b4b
Ryosuke Oguni <r.oguni@unitea.jp>
2022-02-08 11:16:58 +0900

    react-router5->6 ver.UP

--- src/components/templates/Content.js
+++ src/components/templates/Content.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import { Route, Switch, withRouter } from "react-router-dom";
-import routes from "../../routes";
+import { Routes, Route } from "react-router-dom";
+import Manual from "../page/manual";
+import Brand from "../page/brand";
+import Goods from "../page/goods";
 
 function Content() {
   return (
     <Grid container>
-      <Grid sm={2}/>
+      <Grid sm={2} />
       <Grid lg={8} sm={8} spacing={10}>
-        <Switch>
-          {routes.map((route, idx) => (
-            <Route
-              path={route.path}
-              exact={route.exact}
-              component={route.component}
-              key={idx}
-            />
-          ))}
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Manual />} />
+          <Route path="/goods" element={<Goods />} />
+          <Route path="/brand" element={<Brand />} />
+        </Routes>
       </Grid>
     </Grid>
   );
 }
-export default withRouter(Content);
+export default Content;
